b9cca20a0348ece1b448f227701747df51906108
lysunht <lysunhtp1@hotmail.com>
2017-10-04 04:35:41 +0800

    add eslint; add more tests

--- test/test.js
+++ test/test.js
@@ -24,4 +24,34 @@ describe('Liqui', () => {
     let trades = await liqui.trades('eth_btc')
     trades.should.not.null
   })
+
+  it('getInfo', async () => {
+    let privateInfo = await liqui.getInfo('eth_btc')
+    privateInfo.should.not.null
+  })
+
+  it('trade', async () => {
+    let trade = await liqui.trade('eth_btc')
+    trade.should.not.null
+  })
+
+  it('activeOrders', async () => {
+    let orders = await liqui.activeOrders('eth_btc')
+    orders.should.not.null
+  })
+
+  it('orderInfo', async () => {
+    let order = await liqui.orderInfo(1234)
+    order.should.not.null
+  })
+
+  it('cancelOrder', async () => {
+    let canceledOrder = await liqui.cancelOrder(1234)
+    canceledOrder.should.not.null
+  })
+
+  it('tradeHistory', async () => {
+    let history = await liqui.tradeHistory()
+    history.should.not.null
+  })
 })
